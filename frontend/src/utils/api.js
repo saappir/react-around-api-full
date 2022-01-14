061c8761ff8af5d9990fb0597888f40be45175f0
saappir <sapirtzur6@gmail.com>
2022-01-14 14:55:16 +0200

    add localhost

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -63,7 +63,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: "https://api.saappir.students.nomoreparties.sbs",
+  baseUrl: 'http://localhost:3000/',
   headers: {
     Authorization: `Bearer ${localStorage.getItem('token')}`,
     'Content-Type': 'application/json',
