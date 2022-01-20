6b2114183adfcc26acc1fda40c8199af4473ed8e
saappir <sapirtzur6@gmail.com>
2022-01-20 13:43:54 +0200

    f

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:3000/aroundb');
+mongoose.connect('mongodb://localhost:27017/aroundb');
 
 const notFound = (req, res, next) => {
   res.status(404).send({ message: 'Page not found' });
