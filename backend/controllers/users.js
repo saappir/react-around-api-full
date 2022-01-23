b4f72addc4d8a6d2c84ce98260af4909fdce9f35
saappir <sapirtzur6@gmail.com>
2022-01-23 11:44:03 +0200

    f

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -81,6 +81,7 @@ module.exports.login = (req, res) => {
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.send({ token });
+      res.send({ user });
       res.header('authorization', `Bearer ${token}`);
     })
     .catch((err) => {
