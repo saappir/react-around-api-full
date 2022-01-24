2ebac5d9bdd0627e5281f06a27f3a94e4906ea58
saappir <sapirtzur6@gmail.com>
2022-01-24 13:02:22 +0200

    fix card id

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -7,13 +7,13 @@ function Card(props) {
 
   const currentUser = React.useContext(CurrentUserContext);
 
-  const isOwn = card.owner.ObjectId.str === currentUser._id;
-  console.log(card)
+  const isOwn = card.owner === currentUser._id;
+
   const cardDeleteButtonClassName = (
     `card__delete-button ${isOwn ? 'card__delete-button_state_visible' : 'card__delete-button_state_hidden'}`
   );
 
-  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = card.data.likes.some((i) => i._id === currentUser._id);
   const cardLikeButtonClassName = (
     `card__like-button ${isLiked && 'card__like-button_state_active'}`
   )
@@ -38,19 +38,19 @@ function Card(props) {
         className={cardDeleteButtonClassName}
         onClick={handleDeleteClick} />
       <img
-        src={card.link}
-        alt={card.name}
+        src={card.data.link}
+        alt={card.data.name}
         className="card__image"
         onClick={handleClick} />
       <div className="card__inner">
-        <h2 className="card__title">{card.name}</h2>
+        <h2 className="card__title">{card.data.name}</h2>
         <div className="card__likes-container">
           <button
             aria-label="like"
             type="button"
             className={cardLikeButtonClassName}
             onClick={handleCardLike} />
-          <span className="card__likes-count">{card.likes.length}</span>
+          <span className="card__likes-count">{card.data.likes.length}</span>
         </div>
       </div>
     </article>
